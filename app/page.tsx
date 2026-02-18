d9cc24ece3873748ff955b21a441f002392f30ac
Henrique Antony <henriqueantonydev@gmail.com>
2026-02-18 10:37:29 -0300

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import FinanceDashboard from "@/components/FinanceDashboard";
+<link rel="icon" type="image/png" href="/favicon.png" />
 
 export default function Home() {
   return <FinanceDashboard />;
